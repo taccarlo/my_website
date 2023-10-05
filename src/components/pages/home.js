cca3c857cda7b635d2b6cdddbbb35806bab12b35
taccarlo <tacchella.carlo@gmail.com>
2023-10-05 16:39:47 +0200

    Update home.js

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -10,6 +10,12 @@ function Home(){
             <meta name="robots" content="noindex"/>
         </Helmet>
          <h1>My Home Page v. 0.0.1</h1>
+
+         <h2>Github</h2>
+         <h2>GTSA</h2>
+         <h2>Didattica</h2>
+         <h3>Link a canale Youtube</h3>
+         <h3>Educazione civica 23/24</h3>
          <p>Go to <Link to="/about">about</Link></p>
        </div>
      ); 
